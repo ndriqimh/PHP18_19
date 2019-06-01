bff7b08e750132fa3b43992c40cd076e3d631aee
Ndriqim Muhadri <ditimuhadri@gmail.com>
2019-06-01 23:07:42 +0200

    ErrorHandling

---
+++ ProjektiPHP/Errori2.php
@@ -0,0 +1,11 @@
+        <?php
+    function Errori($errno, $errstr) {
+  echo "<b>Error:</b> [$errno] $errstr";
+}
+
+//Vendosja e Error Handler
+set_error_handler("Errori");
+
+//trigger error
+echo($test);
+?>
--- ProjektiPHP/lexim.php
+++ ProjektiPHP/lexim.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <body>
+<form action="Errori2.php" method="post">
+    <button type="submit">ErrorHandlerF</button>
+</form>
 <?php
 /*6 Perdorimi i funksioneve per shkrim dhe lexim nga fajllat,
 implementimi ne projekt i nje shembulli te tille.*/
@@ -8,7 +11,14 @@ implementimi ne projekt i nje shembulli te tille.*/
 $fajlli=fopen("Teksti TCP.txt",'r') or die('Nuk u mundesua hapja');
 echo fread($fajlli,filesize('Teksti TCP.txt'));
 fclose($fajlli);
+//Error Handler Tipi die()
+if(!file_exists('Teksti.txt')){
+    die('Fajlli nuk u gjet');
+}else{
+    $fajlli=fopen('Teksti.txt','r');
+}
 
 ?>
+
 </body>
 </html>
